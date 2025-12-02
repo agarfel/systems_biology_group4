690e4dd9be42540a3ebb1b2318a1b0addecc2162
agarfel <agarfel7@gmail.com>
2025-12-02 20:55:25 +0100

    Added more analysis idk if it works

---
+++ 99_proj_func.R
@@ -0,0 +1,32 @@
+dexseq_analysis <- function(isoCm, Metadata, IsoformInfo){
+  n_samples <- ncol(isoCm)
+  isoCm <- isoCm[rowSums(isoCm >= 10) >= (0.5 * n_samples), ]
+  isoforms <- rownames(isoCm)
+  IsoformInfo <- IsoformInfo |> filter(transcript_id %in% isoforms)
+  IsoformInfo <- IsoformInfo |> distinct(transcript_id, .keep_all = TRUE)
+  IsoformInfo <- IsoformInfo[match(rownames(isoCm),
+                                   IsoformInfo$transcript_id),]
+  stopifnot( all(rownames(isoCm) == IsoformInfo$transcript_id) )
+  
+  
+  Metadata$mutation <- factor(Metadata$mutation)
+  
+  dxdSubset <- DEXSeqDataSet(
+    countData = isoCm,
+    sampleData = Metadata,
+    design = ~ mutation + exon + exon:mutation,
+    featureID = IsoformInfo$transcript_id,
+    groupID = IsoformInfo$gene_id
+  )
+  
+  dxdSubset <- estimateSizeFactors(dxdSubset)
+  dxdSubset <- estimateDispersions(dxdSubset)
+  dxdSubset <- testForDEU(
+    dxdSubset,
+    fullModel = ~ exon + exon:mutation,
+    reducedModel = ~ exon 
+  )
+  dxdSubset <- estimateExonFoldChanges(dxdSubset, fitExpToVar = "mutation")
+  dxdResult <- DEXSeqResults(dxdSubset)
+  return(dxdResult)
+}
