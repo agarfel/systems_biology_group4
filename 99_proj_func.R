d767740659842f502950a0dbf6b113af920a21fd
agarfel <agarfel7@gmail.com>
2025-12-04 13:00:55 +0100

    Compared Pathways

--- 99_proj_func.R
+++ 99_proj_func.R
@@ -30,3 +30,27 @@ dexseq_analysis <- function(isoCm, Metadata, IsoformInfo){
   dxdResult <- DEXSeqResults(dxdSubset)
   return(dxdResult)
 }
+
+pcoa_plot <- function(isoCm, Metadata, IsoformInfo){
+  n_samples <- ncol(isoCm)
+  isoCm <- isoCm[rowSums(isoCm >= 10) >= (0.5 * n_samples), ]
+  isoforms <- rownames(isoCm)
+  IsoformInfo <- IsoformInfo |> filter(transcript_id %in% isoforms)
+  IsoformInfo <- IsoformInfo |> distinct(transcript_id, .keep_all = TRUE)
+  IsoformInfo <- IsoformInfo[match(rownames(isoCm),
+                                   IsoformInfo$transcript_id),]
+  stopifnot( all(rownames(isoCm) == IsoformInfo$transcript_id) )
+  
+  
+  Metadata$mutation <- factor(Metadata$mutation)
+  
+  dxdSubset <- DEXSeqDataSet(
+    countData = isoCm,
+    sampleData = Metadata,
+    design = ~ mutation + exon + exon:mutation,
+    featureID = IsoformInfo$transcript_id,
+    groupID = IsoformInfo$gene_id
+  )
+  
+  plotPCA(rlog(dds_ocular), intgroup=c("mutation"))
+}
